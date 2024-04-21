f03f27d9fdcd4cb9a1bdfff54c347de60d9be9eb
AcademicTheorist <163300085+AcademicTheorist@users.noreply.github.com>
2024-04-21 20:09:35 +0800

    Update about.js

--- about.js
+++ about.js
@@ -4,5 +4,5 @@ const homeButton = document.getElementById("homeButton");
 // Add click event listener to the home button
 homeButton.addEventListener("click", function() {
     // Redirect to the home page (assuming 'home.html' is your home page)
-    window.location.href = "Home.html";
+    window.location.href = "home.html";
 });
