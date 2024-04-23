04281a0caec6ee199b3e144937d021de0cfcd024
Gabes28 <166295265+Gabes28@users.noreply.github.com>
2024-04-23 16:25:09 +0800

    Delete about.js

--- about.js
+++
@@ -1,8 +0,0 @@
-// Get the home button element
-const homeButton = document.getElementById("homeButton");
-
-// Add click event listener to the home button
-homeButton.addEventListener("click", function() {
-    // Redirect to the home page (assuming 'home.html' is your home page)
-    window.location.href = "home.html";
-});
